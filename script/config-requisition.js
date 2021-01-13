1b35d26288d568bc2526f02772b1e8a2d110913c
CaioDePaula <caio.teodoro@ftdata.com.br>
2021-01-13 17:17:46 -0300

    Removendo credencias API

--- script/config-requisition.js
+++ script/config-requisition.js
@@ -1,6 +1,6 @@
 const credentials = {
-    apiKeyPublic: "46113929393e0a1df1a6c13b2960e826",
-    apiKeyPrivate: "a766882da5881030a812869babd4c69f6e4d9116",
+    apiKeyPublic: "",
+    apiKeyPrivate: "",
     url: "http://gateway.marvel.com/v1/public/characters"
 }
 
